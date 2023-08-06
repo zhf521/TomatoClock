b3b3382378e8f0770a6403fcd3411197e51ac5e5
zhf <3407085928@qq.com>
2023-08-06 21:42:55 +0800

    新建項目

---
+++ pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="content">
+		<view class="Clock">
+			时钟
+		</view>
+		<view class="button">
+			<button>开始</button>
+			<button>暂停</button>
+		</view>
+	</view>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style>
+
+</style>
